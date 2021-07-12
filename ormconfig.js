7b1e61db23e24eaf8edf705cd390cd36e23bdc02
Sillas Vidal <sillasv91@gmail.com>
2021-07-12 15:28:31 -0300

    preparing to deploy

--- ormconfig.js
+++ ormconfig.js
@@ -16,9 +16,9 @@ if (process.env.ENV === 'dev') {
   module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
-    // "ssl": {
-    //   "rejectUnauthorized": false
-    // },
+    "ssl": {
+      "rejectUnauthorized": false
+    },
     "entities": ["./dist/modules/**/infra/typeorm/entities/*.js"],
     "migrations": ["./dist/shared/infra/typeorm/migrations/*.js"],
     "cli": {
